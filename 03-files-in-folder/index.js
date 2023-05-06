28a797981568b94f38bf6eaef1ff14f7ec2ef9cf
[Kostiantyn] <[worldairsport@gmail.com]>
2023-05-07 01:32:56 +0300

    feat: add  03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+async function printFilesInfo() {
+  const folderPath = path.join(__dirname, "secret-folder");
+
+  try {
+    const files = await fs.readdir(folderPath);
+
+    for (const fileName of files) {
+      const filePath = path.join(folderPath, fileName);
+      const stats = await fs.stat(filePath);
+
+      if (stats.isFile()) {
+        const fileSizeInBytes = stats.size;
+        const fileSizeInKilobytes = fileSizeInBytes / 1024.0;
+        const fileExtension = path.extname(fileName).replace(".", "");
+        console.log(
+          `${fileName}-${fileExtension}-${fileSizeInKilobytes.toFixed(3)}kb`
+        );
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+printFilesInfo();
