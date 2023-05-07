f26cbdce66e0b9920d6d9369c8afc4b2744c42f6
[Kostiantyn] <[worldairsport@gmail.com]>
2023-05-07 23:06:18 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fsPromises = require("fs").promises;
+const path = require("path");
 
+const sourceDirectoryPath = path.join(__dirname, "files");
+const targetDirectoryPath = path.join(__dirname, "files-copy");
+
+async function copyDirectory() {
+  try {
+    await fsPromises.access(targetDirectoryPath);
+  } catch (error) {
+    await fsPromises.mkdir(targetDirectoryPath);
+  }
+  const files = await fsPromises.readdir(sourceDirectoryPath);
+
+  for (const file of files) {
+    const sourcePath = path.join(sourceDirectoryPath, file);
+    const targetPath = path.join(targetDirectoryPath, file);
+
+    const stats = await fsPromises.stat(sourcePath);
+    if (stats.isFile()) {
+      await fsPromises.copyFile(sourcePath, targetPath);
+    }
+  }
+}
+
+copyDirectory();
